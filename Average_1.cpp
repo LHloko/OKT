c9d28390c4529f6af168deb87349943d98b9765c
Luiz_B <lh.balieiro@gmail.com>
2023-11-23 22:58:36 -0300

    media 1, media 2 e produto simples

---
+++ Average_1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float a,b; cin>>a>>b;
+    float media = ((a*3.5)+(b*7.5))/(3.5+7.5);
+
+    cout<<fixed<<setprecision(5);
+    cout<<"MEDIA = "<<media<<endl;
+
+    return 0;
+}   
---
+++ Average_2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float a,b,c; cin>>a>>b>>c;
+    double media = ((a*2)+(b*3)+(c*5))/10;
+
+    cout<<fixed<<setprecision(1);
+    cout<<"MEDIA = "<<media<<endl;
+
+    return 0;
+}   
