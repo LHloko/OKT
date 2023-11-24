b45505a889b293757465b40e4ef708b003e228ae
Luiz_B <lh.balieiro@gmail.com>
2023-11-23 21:19:19 -0300

    área do circulo

---
+++ area_circulo.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    auto pi =  3.14159;
+    int R; cin>>R;
+
+    cout<<fixed<<setprecision(4);
+    cout<<"A="<<pi*(R*R);
+    //cout<<"A="<<pi*pow(R,2);
+
+    return 0;
+}
